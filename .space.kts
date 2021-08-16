b7bac7e937fcd1086e2defc437051d15f971e41a
dream . <draemwoo@gmail.com>
2021-08-16 07:08:16 +0000

    Change to OpenJDK 9

--- .space.kts
+++ .space.kts
@@ -5,7 +5,7 @@
 */
 
 job("Build and run tests") {
-    container(displayName = "Run gradle build", image = "openjdk:8") {
+    container(displayName = "Run gradle build", image = "openjdk:9") {
         kotlinScript { api ->
             // here can be your complex logic
             api.gradlew("shadowJar")
