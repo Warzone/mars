b99274b1c1c762988039d6e76b848df8689a9c5f
Daniel <danielgulic@gmail.com>
2022-01-02 00:53:13 +1100

    Remove destroyable damage & core damage models

--- src/main/kotlin/network/warzone/mars/api/socket/models/CoreModels.kt
+++ src/main/kotlin/network/warzone/mars/api/socket/models/CoreModels.kt
@@ -5,5 +5,4 @@ import org.bukkit.Material
 import java.util.*
 
 data class CorePartial( val id: String, val name: String, val ownerName: String, val material: Material)
-data class CoreDamageData(val coreId: String, val playerId: UUID)
 data class CoreLeakData(val coreId: String, val contributions: List<Contribution>)
--- src/main/kotlin/network/warzone/mars/api/socket/models/DestroyableModels.kt
+++ src/main/kotlin/network/warzone/mars/api/socket/models/DestroyableModels.kt
@@ -5,5 +5,4 @@ import org.bukkit.Material
 import java.util.*
 
 data class DestroyablePartial(val id: String, val name: String, val ownerName: String, val material: Material, val blockCount: Int )
-data class DestroyableDamageData( val destroyableId: String, val playerId: UUID)
 data class DestroyableDestroyData( val destroyableId: String, val contributions: List<Contribution> )
