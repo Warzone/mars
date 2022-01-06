956f9bc0a520f2cbcd4d5b5f8a16df2755dd614b
Daniel <danielgulic@gmail.com>
2022-01-07 03:59:23 +1100

    Send ended killstreak

--- src/main/kotlin/network/warzone/mars/match/tracker/KillstreakTracker.kt
+++ src/main/kotlin/network/warzone/mars/match/tracker/KillstreakTracker.kt
@@ -14,6 +14,7 @@ import tc.oc.pgm.lib.net.kyori.adventure.text.Component
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.NamedTextColor
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.TextDecoration
 import tc.oc.pgm.util.named.NameStyle
+import java.util.*
 
 class KillstreakTracker : Listener {
     val trackedKillstreaks = mapOf(
@@ -21,23 +22,56 @@ class KillstreakTracker : Listener {
         10 to NamedTextColor.GOLD,
         25 to NamedTextColor.RED,
         50 to NamedTextColor.DARK_AQUA,
-        100 to NamedTextColor.DARK_PURPLE
+        100 to NamedTextColor.DARK_PURPLE,
+        120 to NamedTextColor.BLUE,
+        150 to NamedTextColor.YELLOW,
+        200 to NamedTextColor.BLACK,
     )
+
+    private fun getKillstreak(id: UUID): Int? {
+        val statsModule = MatchManager.getTracker(BigStatsTracker::class)?.matchStatsModule ?: return null
+        val stats = statsModule.getPlayerStat(id)
+        return stats.killstreak
+    }
+
+    private fun getNearestTrackedKillstreak(killstreak: Int): Pair<Int, NamedTextColor> {
+        if (killstreak <= 5) return Pair(5, trackedKillstreaks[5]!!)
+        var nearestKillstreak = killstreak
+        while (trackedKillstreaks[nearestKillstreak] == null) {
+            nearestKillstreak--
+        }
+        return Pair(nearestKillstreak, trackedKillstreaks[nearestKillstreak]!!)
+    }
 
     @EventHandler(priority = EventPriority.HIGH)
     fun onKillstreak(event: MatchPlayerDeathEvent) {
         val killer = event.killer ?: return
         if (!event.isEnemyKill) return
-        val statsModule = MatchManager.getTracker(BigStatsTracker::class)?.matchStatsModule ?: return
-        val stats = statsModule.getPlayerStat(killer)
-        if (!trackedKillstreaks.contains(stats.killstreak)) return
-        event.match.sendMessage(
-            createKillstreakMessage(killer, stats.killstreak)
-        )
+        val killstreak = getKillstreak(killer.id)!!
+        if (!trackedKillstreaks.contains(killstreak)) return
+        event.match.sendMessage(createKillstreakMessage(killer, killstreak))
         ApiClient.emit(
             OutboundEvent.Killstreak,
-            KillstreakData(stats.killstreak, SimplePlayer(killer.id, killer.nameLegacy), false)
+            KillstreakData(killstreak, SimplePlayer(killer.id, killer.nameLegacy), false)
         )
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onKillstreakEnd(event: MatchPlayerDeathEvent) {
+        val victim = event.victim
+        val killer = event.killer ?: return // no killer = no broadcast
+        val killstreak = getKillstreak(victim.id)!!
+        println("Victim killstreak: $killstreak")
+
+        if (killstreak >= 5) {
+            event.match.sendMessage(createKillstreakEndMessage(killer, event.victim.participantState!!, killstreak))
+
+            val nearestKillstreak = getNearestTrackedKillstreak(killstreak).first
+            ApiClient.emit(
+                OutboundEvent.Killstreak,
+                KillstreakData(nearestKillstreak, SimplePlayer(killer.id, killer.nameLegacy), true)
+            )
+        }
     }
 
     private fun createKillstreakMessage(killer: ParticipantState, count: Int): Component {
@@ -46,5 +80,15 @@ class KillstreakTracker : Listener {
             Component.text(count, trackedKillstreaks[count]).decorate(TextDecoration.BOLD)
         return killerComp.append(Component.text(" is on a kill streak of ", NamedTextColor.GRAY)).append(countComp)
             .append(Component.text("!", NamedTextColor.GRAY))
+    }
+
+    private fun createKillstreakEndMessage(ender: ParticipantState, ended: ParticipantState, count: Int): Component {
+        val enderComp = ender.getName(NameStyle.COLOR).decorate(TextDecoration.BOLD)
+        val endedComp = ended.getName(NameStyle.COLOR).decorate(TextDecoration.BOLD)
+        val nearestColor = getNearestTrackedKillstreak(count).second
+        val countComp =
+            Component.text(count, nearestColor).decorate(TextDecoration.BOLD)
+        return enderComp.append(Component.text(" ended ", NamedTextColor.GRAY)).append(endedComp)
+            .append(Component.text("'s killstreak of ", NamedTextColor.GRAY)).append(countComp)
     }
 }
