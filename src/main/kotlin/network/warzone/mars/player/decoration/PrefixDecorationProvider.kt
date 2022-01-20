9547862147e1aeef231768fb4005be624f75da17
jorgeberrex <jorgeberrex@hotmail.com>
2022-01-20 14:50:54 -0500

    Get real prefix from player

--- src/main/kotlin/network/warzone/mars/player/decoration/PrefixDecorationProvider.kt
+++ src/main/kotlin/network/warzone/mars/player/decoration/PrefixDecorationProvider.kt
@@ -1,5 +1,9 @@
 package network.warzone.mars.player.decoration
 
+import kotlinx.coroutines.runBlocking
+import net.md_5.bungee.api.ChatColor
+import network.warzone.mars.player.PlayerManager
+import network.warzone.mars.utils.color
 import tc.oc.pgm.lib.net.kyori.adventure.text.Component.text
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.NamedTextColor
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.TextDecoration
@@ -9,11 +13,13 @@ import java.util.*
 
 class PrefixDecorationProvider : NameDecorationProvider {
 
-    override fun getPrefix(player: UUID?): String = TextTranslations.translateLegacy(
-        // TODO: Get prefix from player ranks
-        text("MOD", NamedTextColor.YELLOW, TextDecoration.BOLD).append(text(" ")), null
-    )
+    override fun getPrefix(uuid: UUID): String = runBlocking {
+        val player = PlayerManager.getPlayer(uuid)
+        val prefix: String = player?.getPrefix() ?: return@runBlocking ""
+        // Add a space after the prefix
+        return@runBlocking prefix.color() + " "
+    }
 
-    override fun getSuffix(p0: UUID?): String = ""
+    override fun getSuffix(uuid: UUID?): String = ""
 
 }
