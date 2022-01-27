18d6020b889f9afbfece11231cbad45bb91b42ce
Daniel <danielgulic@gmail.com>
2022-01-28 10:26:32 +1100

    Block problematic commands

--- src/main/kotlin/network/warzone/mars/commands/CommandModule.kt
+++ src/main/kotlin/network/warzone/mars/commands/CommandModule.kt
@@ -3,6 +3,7 @@ package network.warzone.mars.commands
 import app.ashcon.intake.argument.CommandArgs
 import app.ashcon.intake.parametric.AbstractModule
 import app.ashcon.intake.parametric.Provider
+import network.warzone.mars.Mars
 import network.warzone.mars.broadcast.Broadcast
 import network.warzone.mars.commands.providers.*
 import tc.oc.pgm.lib.net.kyori.adventure.audience.Audience
@@ -11,10 +12,19 @@ import network.warzone.mars.player.models.PlayerProfile
 import network.warzone.mars.punishment.models.Punishment
 import network.warzone.mars.rank.models.Rank
 import network.warzone.mars.tag.models.Tag
+import org.bukkit.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
 import java.util.function.Supplier
 import kotlin.reflect.KClass
 
-object CommandModule : AbstractModule() {
+object CommandModule : AbstractModule(), Listener {
+    init {
+        Mars.registerEvents(this)
+    }
+
     override fun configure() {
         bind(Audience::class, AudienceProvider())
         bind(Rank::class, RankProvider())
@@ -41,6 +51,46 @@ object CommandModule : AbstractModule() {
 
         override fun isProvided(): Boolean {
             return true
+        }
+    }
+
+    val punishCommands = listOf(
+        "/ban",
+        "/mute",
+        "/kick",
+        "/pardon",
+        "/baninfo",
+        "/banip",
+        "/pgm:ban",
+        "/pgm:baninfo",
+        "/pgm:banip",
+        "/pgm:kick",
+        "/pgm:mute",
+        "/warn",
+        "/pgm:warn",
+        "/pgm:mutes",
+        "/mutes",
+        "/pgm:mutelist",
+        "/mutelist",
+        "/permban",
+        "/pgm:permban"
+    )
+
+    val miscCommands =
+        listOf("/msg", "/pgm:msg", "/pgm:tell", "/w", "/pgm:w", "/pm", "/dm", "/pgm:pm", "/pgm:dm", "/tell")
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    fun onCommand(event: PlayerCommandPreprocessEvent) {
+        val isPunishCommand = punishCommands.any { event.message.toLowerCase().startsWith(it) }
+        if (isPunishCommand) {
+            event.isCancelled = true
+            event.player.sendMessage("${ChatColor.RED}This command has been disabled in favour of /punish.")
+        }
+
+        val isBlockedCommand = miscCommands.any { event.message.toLowerCase().startsWith(it) }
+        if (isBlockedCommand) {
+            event.isCancelled = true
+            event.player.sendMessage("${ChatColor.RED}This command is unavailable.")
         }
     }
 }
