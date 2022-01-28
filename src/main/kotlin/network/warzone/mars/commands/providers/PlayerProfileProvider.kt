4554d4f3825ece0e3558020f533157e501f00b50
draem <owen@gimbel.ca>
2022-01-28 10:35:30 -0500

    add suggestions to PlayerProfileProvider

--- src/main/kotlin/network/warzone/mars/commands/providers/PlayerProfileProvider.kt
+++ src/main/kotlin/network/warzone/mars/commands/providers/PlayerProfileProvider.kt
@@ -2,12 +2,14 @@ package network.warzone.mars.commands.providers
 
 import app.ashcon.intake.argument.ArgumentParseException
 import app.ashcon.intake.argument.CommandArgs
+import app.ashcon.intake.argument.Namespace
 import app.ashcon.intake.bukkit.parametric.provider.BukkitProvider
 import com.github.kittinunf.result.getOrNull
 import kotlinx.coroutines.runBlocking
 import network.warzone.mars.player.feature.PlayerFeature
 import network.warzone.mars.player.feature.exceptions.PlayerMissingException
 import network.warzone.mars.player.models.PlayerProfile
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 
 class PlayerProfileProvider : BukkitProvider<PlayerProfile> {
@@ -26,5 +28,16 @@ class PlayerProfileProvider : BukkitProvider<PlayerProfile> {
         profile ?: throw ArgumentParseException(PlayerMissingException(name).asTextComponent().content())
 
         return@runBlocking profile
+    }
+
+    override fun getSuggestions(
+        prefix: String?,
+        namespace: Namespace?,
+        modifiers: MutableList<out Annotation>?
+    ): MutableList<String> {
+        return Bukkit.getOnlinePlayers()
+            .filter { it.name.startsWith(prefix ?: "") }
+            .map { it.name }
+            .toMutableList()
     }
 }
