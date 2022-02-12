c49d7e99f66f08cf63bc28fee127546e0931c80a
jorgeberrex <jorgeberrex@hotmail.com>
2022-02-12 15:02:48 -0500

    Allow using /lookup and /notes as console

--- src/main/kotlin/network/warzone/mars/player/commands/ModCommands.kt
+++ src/main/kotlin/network/warzone/mars/player/commands/ModCommands.kt
@@ -24,7 +24,7 @@ import javax.annotation.Nullable
 
 class ModCommands {
     @Command(aliases = ["lookup", "alts", "lu"], desc = "Lookup player information & alts", usage = "<player>", perms = ["mars.lookup"])
-    fun onPlayerLookup(@Sender sender: CommandSender, audience: Audience, context: PlayerContext, @PlayerName target: String) {
+    fun onPlayerLookup(@Sender sender: CommandSender, audience: Audience, @PlayerName target: String) {
         Mars.async {
             try {
                 val lookup =
@@ -64,7 +64,7 @@ class ModCommands {
                         .append { newline() }
                 }
 
-                context.matchPlayer.sendMessage(message)
+                audience.sendMessage(message)
             } catch (e: FeatureException) {
                 audience.sendMessage(e.asTextComponent())
             }
@@ -79,9 +79,8 @@ class ModCommands {
         perms = ["mars.notes"]
     )
     fun onNotes(
-        @Sender sender: Player,
+        @Sender sender: CommandSender,
         audience: Audience,
-        context: PlayerContext,
         @PlayerName name: String, // Accept offline players too
         @Nullable op: String?,
         @Nullable @Text value: String?
@@ -92,12 +91,16 @@ class ModCommands {
                 null -> {
                     var message = text("Notes for ${profile.name}", NamedTextColor.GREEN).append { newline() }
                     profile.notes.forEach {
-                        message = message.append { it.asTextComponent(profile.name, it.author.id == sender.uniqueId) }
+                        message = message.append { it.asTextComponent(
+                            profile.name,
+                            sender !is Player || it.author.id == sender.uniqueId
+                        ) }
                             .append { newline() }
                     }
-                    context.matchPlayer.sendMessage(message)
+                    audience.sendMessage(message)
                 }
                 "add" -> {
+                    if (sender !is Player) throw CommandException("${ChatColor.RED}You must be a player to add notes.")
                     if (value == null || value.trim().isEmpty()) throw CommandException("No note content provided")
                     PlayerFeature.addNote(profile.name, value.trim(), SimplePlayer(sender.uniqueId, sender.name))
                     sender.sendMessage("${ChatColor.GREEN}Added note")
@@ -107,7 +110,7 @@ class ModCommands {
                     try {
                         val noteId = value.toInt()
                         val note = profile.notes.find { it.id == noteId } ?: throw CommandException("Invalid note ID")
-                        if (note.author.id != sender.uniqueId) throw CommandException("You cannot delete this note")
+                        if (sender is Player && note.author.id != sender.uniqueId) throw CommandException("You cannot delete this note")
 
                         PlayerFeature.removeNote(profile.name, noteId)
                         sender.sendMessage("${ChatColor.GREEN}Deleted note #$noteId")
