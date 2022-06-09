f48758462b6351d8943e730ea6c0ee5758ce78d3
Mew2K <72997300+Mew2K@users.noreply.github.com>
2022-06-09 14:22:24 -0500

    /tp command
    Allows spectators to use /tp as a tool for spectating.
    
    (Note: Some of this code is definitely redundant, and the message feedback is formatted in plain-white text, but other than that, the functionality works).

--- src/main/kotlin/network/warzone/mars/player/commands/MiscCommands.kt
+++ src/main/kotlin/network/warzone/mars/player/commands/MiscCommands.kt
@@ -5,13 +5,18 @@ import app.ashcon.intake.CommandException
 import app.ashcon.intake.bukkit.parametric.annotation.Sender
 import network.warzone.mars.Mars
 import network.warzone.mars.commands.providers.PlayerName
+import network.warzone.mars.match.models.PartyData
 import network.warzone.mars.match.tracker.KillstreakTracker
+import network.warzone.mars.player.PlayerContext
 import network.warzone.mars.utils.matchPlayer
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.GameMode
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import tc.oc.pgm.api.party.Competitor
+import tc.oc.pgm.api.party.Party
+import tc.oc.pgm.api.player.ParticipantState
 import tc.oc.pgm.lib.net.kyori.adventure.text.Component
 import tc.oc.pgm.lib.net.kyori.adventure.text.event.ClickEvent
 import tc.oc.pgm.lib.net.kyori.adventure.text.format.NamedTextColor
@@ -58,5 +63,45 @@ class MiscCommands {
         val ping = player.spigot().ping
         if (player == sender) sender.sendMessage("${ChatColor.GRAY}Your ping is ${ChatColor.AQUA}${ping}ms")
         else sender.sendMessage("${ChatColor.AQUA}$possessive ${ChatColor.GRAY}ping is ${ChatColor.AQUA}${ping}ms")
+    }
+
+    @Command(
+        aliases = ["tp"],
+        desc = "Teleport to a player",
+        usage = "<player> [otherPlayer]",
+        perms = ["mars.tp", "mars.tp.other"])
+    fun onTeleportToPlayer(
+        @Sender sender: Player,
+        @Nullable @PlayerName playerOneName: String?,
+        @Nullable @PlayerName playerTwoName: String?)
+    {
+        val targetPlayerOne = playerOneName?.let { Bukkit.getPlayer(it) }
+        val targetPlayerTwo = playerTwoName?.let { Bukkit.getPlayer(it) }
+
+        // Prevent even mods from using command if they are participating
+        // in the match. Opped players do not apply.
+        if (sender.matchPlayer.isParticipating && !sender.isOp) {
+            sender.sendMessage("You cannot use this command while participating!")
+            return
+        }
+        // Scenario One: Both arguments are empty.
+        if (targetPlayerOne == null) {
+            sender.sendMessage("You must provide a player to teleport to!")
+            return
+        }
+        // Scenario Two: Only argument two is empty.
+        if (targetPlayerTwo == null) {
+            sender.teleport(targetPlayerOne.location)
+            sender.sendMessage("Teleported to " + playerOneName)
+            return
+        }
+        // Scenario Three: Both arguments are filled.
+        if (sender.hasPermission("mars.tp.other")) {
+            targetPlayerOne.teleport(targetPlayerTwo.location)
+            sender.sendMessage("Teleported " + playerOneName + " to " + playerTwoName)
+            return
+        }
+
+        sender.sendMessage("If you see this message, something has gone wrong.")
     }
 }
