0fc9916831f20e6fef052acffcaf823f0163b30b
Daniel <danielgulic@gmail.com>
2022-12-04 18:42:55 +1100

    Refactor permissions

--- src/main/kotlin/network/warzone/mars/admin/AdminCommands.kt
+++ src/main/kotlin/network/warzone/mars/admin/AdminCommands.kt
@@ -14,6 +14,10 @@ import tc.oc.pgm.api.Permissions
 import tc.oc.pgm.lib.cloud.commandframework.exceptions.NoPermissionException
 import javax.annotation.Nullable
 
+const val XP_MULTIPLIER_VIEW_PERMISSION = "mars.xpmultiplier.view"
+const val XP_MULTIPLIER_SET_PERMISSION = "mars.xpmultiplier.set"
+const val XP_MULTIPLIER_SET_UNLIMITED_PERMISSION = "mars.xpmultiplier.setunlimited"
+
 class AdminCommands {
     /**
      * View active XP multiplier: mars.xpmultiplier.view
@@ -21,7 +25,7 @@ class AdminCommands {
      * Set XP multiplier to value 1-5: mars.xpmultiplier.set
      * Set XP multiplier to value >5: mars.xpmultiplier.setunlimited && mars.xpmultiplier.set
      */
-    @Command(aliases = ["xpmultiplier", "booster"], desc = "Set an XP multiplier", usage = "[multiplier]", perms = ["mars.xpmultiplier.view"])
+    @Command(aliases = ["xpmultiplier", "booster"], desc = "Set an XP multiplier", usage = "[multiplier]", perms = [XP_MULTIPLIER_VIEW_PERMISSION])
     fun onSetXPMultiplier(@Sender sender: CommandSender, @Nullable multiplier: Float?) {
         Mars.async {
             try {
@@ -31,17 +35,17 @@ class AdminCommands {
                     val player = xpMultiplier.player
 
                     var message = "${ChatColor.GREEN}An XP multiplier of ${ChatColor.YELLOW}${xpMultiplier.value}x ${ChatColor.GREEN}is active"
-                    if (sender.hasPermission(Permissions.STAFF)) message += ", set ${ChatColor.YELLOW}${xpMultiplier.updatedAt.getRelativeTime(null, true)}${ChatColor.GREEN}"
-                    if (sender.hasPermission(Permissions.STAFF) && player != null) message += " by ${ChatColor.YELLOW}${player.name}"
+                    if (sender.hasPermission(XP_MULTIPLIER_SET_PERMISSION)) message += ", set ${ChatColor.YELLOW}${xpMultiplier.updatedAt.getRelativeTime(null, true)}${ChatColor.GREEN}"
+                    if (sender.hasPermission(XP_MULTIPLIER_SET_PERMISSION) && player != null) message += " by ${ChatColor.YELLOW}${player.name}"
 
                     sender.sendMessage(message)
                 } else {
-                    if (!sender.hasPermission("mars.xpmultiplier.set")) throw CommandException("Insufficient permissions")
-                    if (multiplier > 5f && !sender.hasPermission("mars.xpmultiplier.setunlimited")) throw CommandException("Insufficient permissions")
+                    if (!sender.hasPermission(XP_MULTIPLIER_SET_PERMISSION)) throw CommandException("Insufficient permissions")
+                    if (multiplier > 5f && !sender.hasPermission(XP_MULTIPLIER_SET_UNLIMITED_PERMISSION)) throw CommandException("Insufficient permissions")
                     if (multiplier < 1) throw CommandException("XP multiplier cannot be less than one")
                     val events = AdminService.setXPMultiplier(multiplier, if (sender is Player) SimplePlayer(sender.uniqueId, sender.name) else null)
                     val xpMultiplier = events?.xpMultiplier ?: return@async sender.sendMessage("${ChatColor.GREEN}XP multiplier cleared")
-                    Bukkit.broadcast("${ChatColor.GREEN}XP multiplier has been set to ${ChatColor.YELLOW}${xpMultiplier.value}x ${ChatColor.GREEN}by ${ChatColor.YELLOW}${sender.name}", Permissions.STAFF)
+                    Bukkit.broadcast("${ChatColor.GREEN}XP multiplier has been set to ${ChatColor.YELLOW}${xpMultiplier.value}x ${ChatColor.GREEN}by ${ChatColor.YELLOW}${sender.name}", XP_MULTIPLIER_SET_PERMISSION)
                 }
             } catch (e: CommandException) {
                 sender.sendMessage("${ChatColor.RED}${e.message}")
