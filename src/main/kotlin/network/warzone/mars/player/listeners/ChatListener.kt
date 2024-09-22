10772d1625ffb7894ae53d529594bfe7794ad43f
jorgeberrex <jorgeberrex@hotmail.com>
2024-09-21 20:10:30 -0500

    Ignore cancelled chat message events
    Improves compatibility with chat filters

--- src/main/kotlin/network/warzone/mars/player/listeners/ChatListener.kt
+++ src/main/kotlin/network/warzone/mars/player/listeners/ChatListener.kt
@@ -118,7 +118,7 @@ class ChatListener : Listener {
         sendAdminChat(PGM.get().matchManager.getMatch(), prefix, player.name, message, serverId)
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     fun onPlayerChat(event: AsyncPlayerChatEvent) = runBlocking {
         val player = event.player
         val context = PlayerManager.getPlayer(player.uniqueId)!!
