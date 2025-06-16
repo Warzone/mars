a4720addfd8ea6ea69e3f405107bca11b86112ce
MewTwoKing <MewTwoKing5@gmail.com>
2025-06-16 15:30:37 -0500

    Implement chair rotation

---
+++ src/main/kotlin/network/warzone/mars/sit/ChairRotationTask.kt
@@ -0,0 +1,32 @@
+package network.warzone.mars.sit
+
+import network.warzone.mars.Mars
+import org.bukkit.Bukkit
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftArmorStand
+import org.bukkit.scheduler.BukkitRunnable
+
+class ChairRotationTask : BukkitRunnable() {
+
+    val sitController = Mars.sitController
+
+    override fun run() {
+        for ((uuid, armorStand) in sitController.sitting) {
+            val player = Bukkit.getPlayer(uuid) ?: continue
+            if (armorStand == null || !player.isOnline) continue
+
+            val entityArmorStand = (armorStand as CraftArmorStand).handle
+            entityArmorStand.yaw = player.location.yaw
+            entityArmorStand.setPositionRotation(
+                entityArmorStand.locX,
+                entityArmorStand.locY,
+                entityArmorStand.locZ,
+                player.location.yaw,
+                entityArmorStand.pitch
+            )
+        }
+    }
+
+    fun start() {
+        this.runTaskTimer(Mars.get(), 0L, 1L)
+    }
+}
